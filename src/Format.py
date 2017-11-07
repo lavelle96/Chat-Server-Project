945478eaa14a46f16900fbb134d26322aa29322c
Daniel Lavelle <lavelld@tcd.ie>
2017-11-07 19:36:39 +0000

    Adding space to chat message :(

--- src/Format.py
+++ src/Format.py
@@ -1,2 +1,2 @@
 def chat_format(room_reference, client_name, message):
-    return "CHAT: " + room_reference + "\nCLIENT_NAME: " + client_name + '\nMESSAGE: ' + message + '\n'
+    return "CHAT: " + room_reference + "\nCLIENT_NAME: " + client_name + '\nMESSAGE: ' + message + '\n\n'
